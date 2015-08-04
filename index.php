f3c37be27911a381cd1ac0196dc15e3a81701660
ewieser <ewald.wieser@fhstp.ac.at>
2015-08-04 12:45:17 +0200

    Removed video from index.php

--- index.php
+++ index.php
@@ -34,17 +34,6 @@
 	</div>
 </div>
 
-
-<div id="video">
-	<p>Here is a little showcase of our annotation web tool:</p>
-	<video id="player" class="projekktor" title="Webtool Demovideo" poster="video/screenshot_640.png" width="640" height="360" controls>
-	<source src="video/webtool_demo_mq.mp4" type="video/mp4">
-	<source src="video/webtool_demo_mq.webm" type="video/webm">
-	<source src="video/webtool_demo_mq.ogg" type="video/ogg">
-	Unfortunately your browser does not support HTML5!
-	</video>
-</div>
-
 <?php include('footer_inc.php'); ?>
 
 
